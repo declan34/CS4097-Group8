ae9ce228bac248ffeda1c3f9cf2300e221f66b78
Declan Ortinau <declan@ortinau.com>
2023-03-08 00:23:23 -0600

    non working submit and reset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,8 +14,33 @@ public class GameManager : MonoBehaviour
 		Board = new char[15, 15];
 	}
 
+	public void reset()
+	{
+		Board = new char[15, 15];
+	}
+
 	public void submit()
 	{
-
+		Debug.Log("OnSubmit");
+		Debug.Log(Board);
+		// Validation and Scoring
+		for(int i = 0; i < 15; i++)
+		{
+			for(int j = 0; j < 15; j++) 
+			{
+				if (Board[i, j].CompareTo(null) == 0) // meaning there is a letter
+				{
+					// Check Horizontal
+					if (Board[i+1,j].CompareTo(null) == 0)
+					{
+						Debug.Log("Horizontal Word");
+					}
+					else if (Board[i, j+1].CompareTo(null) == 0)
+					{
+						Debug.Log("Vertical Word");
+					}
+				}
+			}
+		}
 	}
 }
